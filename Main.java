3563a19671b18e183d6bf1716b93e461e57898e2
Venkata Mahesh Babu Korrapati <korrapav@mail.gvsu.edu>
2024-11-21 17:48:35 -0600

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    /* day -02 (19/11/24) -
+     * Topics : 
+     * Object - a real instance in of a class which we call a blueprint in java.
+     * Polymorphism : 
+     * - 
+     */
+   public static void main(String[] args){
+
+   } 
+}
