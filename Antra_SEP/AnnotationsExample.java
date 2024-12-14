b455957c6283318e9da0d7385a00330e198a7ad5
Venkata Mahesh Babu Korrapati <korrapav@mail.gvsu.edu>
2024-12-13 18:09:28 -0600

    Deletion files in AntraSEP

---
+++ Antra_SEP/AnnotationsExample.java
@@ -0,0 +1,12 @@
+public class AnnotationsExample {
+    /*
+     * Network and Server in Java : 
+     *      - "Request-response cycle" concept. (Refer GPT Day-11)
+     *      -  Apache Tomcat server => Apache Tomcat is an open-source Java Servlet Container and Web Server developed by the Apache Software Foundation.
+     *                                 It is used to deploy and run Java-based web applications and provides a "pure Java" HTTP server environment for running Java Servlets, Java Server Pages (JSP), and Java Expression Language (EL).
+     *                                 Tomcat acts as a web server for dynamic Java web applications, but it doesn't replace a full-fledged web server like Apache HTTP Server or Nginx; it focuses on the execution of Java Servlets and JSPs.
+     *      - How Does Tomcat works : request -> receives or listens for HTTP request using on a specified port (default : 8080) by 'Coyote'(An HTTP Connector component) 
+                                      - > 'servelet container' if maps to Javaservlet or  'processing JSP' if maps to JSP (Java Servlet Page)  - > response generation.
+            -                                                                                                                                  
+     */
+}
