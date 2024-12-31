4bf8289279c3b6af0d5151459ed49478dc270006
Venkata Mahesh Babu Korrapati <korrapav@mail.gvsu.edu>
2024-12-31 15:24:15 -0600

    StringMastery class updates

--- Core_Java/The_Java_Language/Basics/StringMastery.java
+++ Core_Java/The_Java_Language/Basics/StringMastery.java
@@ -7,5 +7,19 @@ import java.lang.StringTemplate;
 import java.lang.StringIndexOutOfBoundsException;
 
 class StringMastery{
-    
+    /*
+     * Part of "Java.lang" package.
+     * Immutable class -> any modification to an object in turn creates a new object.
+     * String pool -> a special memory in heap region which stores string literals which allows java to optimize memory usage by reusing instances.
+     * Implements -> String class implements Serializable, Comparable<String> , CharSequesnce interfaces.
+     * 
+     * --------------------Constructor ways to create instances------------------------
+     * 1. From a literal -> String str = "Hello"
+     * 2. From a character array -> char[] charArr = new char{'H','e','l','l','o'}; String str = new String(charArr);
+     * 3. From a byte array -> byte[] byteArr = new byte{72,101,108,108,111} (ASCII values of H,e,l,l,o); String str = new String(byteArr);
+     * 4. From another string -> String str = new String("Hello");
+     * , etc. (In practical string class provides 10 different constructors which include above to create string instances for different scenarios).
+     * --------------------------------------------------------------------------------
+     * 
+     */
 }
